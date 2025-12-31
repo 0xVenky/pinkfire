50de9c677d6c85204ffc0680f06d2985dea91b33
venky <venky.sridh@gmail.com>
2025-12-31 15:02:24 +0100

    add inflation chart

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { StatCard } from './StatCard';
 export { BurnChart } from './BurnChart';
 export { RefreshIndicator } from './RefreshIndicator';
 export { Footer } from './Footer';
+export { InflationChart } from './InflationChart';
